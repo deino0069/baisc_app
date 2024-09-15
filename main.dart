f2e27acf0384f98de0f2b81ed910391e94954542
deino0069 <163132919+deino0069@users.noreply.github.com>
2024-09-15 11:32:28 +0530

    main.dart
    Main file from where the app begins.

---
+++ main.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+import "package:first_app/container.dart";
+
+void main() {
+  runApp(
+   const MaterialApp(
+      home: Scaffold(
+        body: MyGredient(
+           [
+            Color.fromARGB(255, 0, 109, 205),
+            Color.fromARGB(255, 252, 252, 252),
+          ],
+        ),
+      ),
+    ),
+  );
+}
